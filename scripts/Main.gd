a878efb9c9cb1fc68b54d31f1d5c42dcb177856f
MariabrosKC <maria.brosnan@kerrycollege.ie>
2023-02-07 10:21:36 +0000

    godot game dodge creeps updated

---
+++ scripts/Main.gd
@@ -0,0 +1,31 @@
+extends Node
+
+export (PackedScene) var mob_scene
+
+
+func _ready():
+	randomize()
+	$UserInterface/Retry.hide()
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		get_tree().reload_current_scene()
+
+
+func _on_MobTimer_timeout():
+	var mob = mob_scene.instance()
+
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	mob_spawn_location.unit_offset = randf()
+
+	var player_position = $Player.transform.origin
+	mob.initialize(mob_spawn_location.translation, player_position)
+
+	add_child(mob)
+	mob.connect("squashed", $UserInterface/ScoreLabel, "_on_Mob_squashed")
+
+
+func _on_Player_hit():
+	$MobTimer.stop()
+	$UserInterface/Retry.show()
---
+++ scripts/Player.gd
@@ -0,0 +1,65 @@
+extends KinematicBody
+
+# Emitted when the player was hit by a mob.
+signal hit
+
+# How fast the player moves in meters per second.
+export var speed = 14
+# The downward acceleration when in the air, in meters per second per second.
+export var fall_acceleration = 75
+# Vertical impulse applied to the character upon jumping in meters per second.
+export var jump_impulse = 20
+# Vertical impulse applied to the character upon bouncing over a mob in meters per second.
+export var bounce_impulse = 16
+
+var velocity = Vector3.ZERO
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
+		#$player.playback_speed = 4
+	#else:
+		#$player.playback_speed = 1
+
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+
+	# Jumping
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y += jump_impulse
+
+	velocity.y -= fall_acceleration * delta
+	velocity = move_and_slide(velocity, Vector3.UP)
+
+	for index in range(get_slide_count()):
+		var collision = get_slide_collision(index)
+		if collision.collider.is_in_group("mob"):
+			print("Collided with monster")
+			var mob = collision.collider
+			if Vector3.UP.dot(collision.normal) > 0.1:
+				mob.squash()
+				velocity.y = bounce_impulse
+
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
+
+
+func die():
+	emit_signal("hit")
+	queue_free()
+
+
+func _on_MobDetector_body_entered(_body):
+	die()
